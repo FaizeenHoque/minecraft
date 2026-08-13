a0cde98e42be5641c32f9cc2c404fb3dc5928da3
Faizeen Hoque <haquefaizeen11@gmail.com>
2026-08-13 07:14:23 +0600

    Refactor VAO class to support flexible attribute linking; update vertex data and shaders for color handling

--- src/vao.h
+++ src/vao.h
@@ -10,7 +10,7 @@ public:
     GLuint ID;
     VAO();
 
-    void LinkVBO(VBO &VBO, GLuint layout);
+    void LinkAttrib(VBO &VBO, GLuint layout, GLenum numComponents, GLenum type, GLsizeiptr stride, void *offset);
 
     // Binds the VAO
     void Bind();
