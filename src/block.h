c252d1ad7e48a952d1a89df5cc204640de7a7752
Faizeen Hoque <haquefaizeen11@gmail.com>
2026-08-13 12:29:51 +0600

    Add Bedrock block type and update main rendering logic

--- src/block.h
+++ src/block.h
@@ -8,7 +8,8 @@ enum class BlockType
 {
     Grass,
     Dirt,
-    Stone
+    Stone,
+    Bedrock
 };
 
 struct BlockTexture
--- src/main.cpp
+++ src/main.cpp
@@ -19,71 +19,63 @@
 const unsigned int width = 1600;
 const unsigned int height = 800;
 
-GLfloat vertices[] =
-    {
-        // Position              Color              TexCoord
+GLfloat vertices[] = {
+    // Position              Color              TexCoord
 
-        // Front
-        -0.5f, -0.5f, 0.5f, 1, 0, 0, 0, 0,
-        0.5f, -0.5f, 0.5f, 0, 1, 0, 1, 0,
-        0.5f, 0.5f, 0.5f, 0, 0, 1, 1, 1,
-        -0.5f, 0.5f, 0.5f, 1, 1, 0, 0, 1,
+    // Front
+    -0.5f, -0.5f, 0.5f, 1, 0, 0, 0, 0,
+    0.5f, -0.5f, 0.5f, 0, 1, 0, 1, 0,
+    0.5f, 0.5f, 0.5f, 0, 0, 1, 1, 1,
+    -0.5f, 0.5f, 0.5f, 1, 1, 0, 0, 1,
 
-        // Back
-        0.5f, -0.5f, -0.5f, 1, 0, 0, 0, 0,
-        -0.5f, -0.5f, -0.5f, 0, 1, 0, 1, 0,
-        -0.5f, 0.5f, -0.5f, 0, 0, 1, 1, 1,
-        0.5f, 0.5f, -0.5f, 1, 1, 0, 0, 1,
+    // Back
+    0.5f, -0.5f, -0.5f, 1, 0, 0, 0, 0,
+    -0.5f, -0.5f, -0.5f, 0, 1, 0, 1, 0,
+    -0.5f, 0.5f, -0.5f, 0, 0, 1, 1, 1,
+    0.5f, 0.5f, -0.5f, 1, 1, 0, 0, 1,
 
-        // Right
-        0.5f, -0.5f, 0.5f, 1, 0, 0, 0, 0,
-        0.5f, -0.5f, -0.5f, 0, 1, 0, 1, 0,
-        0.5f, 0.5f, -0.5f, 0, 0, 1, 1, 1,
-        0.5f, 0.5f, 0.5f, 1, 1, 0, 0, 1,
+    // Right
+    0.5f, -0.5f, 0.5f, 1, 0, 0, 0, 0,
+    0.5f, -0.5f, -0.5f, 0, 1, 0, 1, 0,
+    0.5f, 0.5f, -0.5f, 0, 0, 1, 1, 1,
+    0.5f, 0.5f, 0.5f, 1, 1, 0, 0, 1,
 
-        // Left
-        -0.5f, -0.5f, -0.5f, 1, 0, 0, 0, 0,
-        -0.5f, -0.5f, 0.5f, 0, 1, 0, 1, 0,
-        -0.5f, 0.5f, 0.5f, 0, 0, 1, 1, 1,
-        -0.5f, 0.5f, -0.5f, 1, 1, 0, 0, 1,
+    // Left
+    -0.5f, -0.5f, -0.5f, 1, 0, 0, 0, 0,
+    -0.5f, -0.5f, 0.5f, 0, 1, 0, 1, 0,
+    -0.5f, 0.5f, 0.5f, 0, 0, 1, 1, 1,
+    -0.5f, 0.5f, -0.5f, 1, 1, 0, 0, 1,
 
-        // Top
-        -0.5f, 0.5f, 0.5f, 1, 0, 0, 0, 0,
-        0.5f, 0.5f, 0.5f, 0, 1, 0, 1, 0,
-        0.5f, 0.5f, -0.5f, 0, 0, 1, 1, 1,
-        -0.5f, 0.5f, -0.5f, 1, 1, 0, 0, 1,
+    // Top
+    -0.5f, 0.5f, 0.5f, 1, 0, 0, 0, 0,
+    0.5f, 0.5f, 0.5f, 0, 1, 0, 1, 0,
+    0.5f, 0.5f, -0.5f, 0, 0, 1, 1, 1,
+    -0.5f, 0.5f, -0.5f, 1, 1, 0, 0, 1,
 
-        // Bottom
-        -0.5f, -0.5f, -0.5f, 1, 0, 0, 0, 0,
-        0.5f, -0.5f, -0.5f, 0, 1, 0, 1, 0,
-        0.5f, -0.5f, 0.5f, 0, 0, 1, 1, 1,
-        -0.5f, -0.5f, 0.5f, 1, 1, 0, 0, 1};
+    // Bottom
+    -0.5f, -0.5f, -0.5f, 1, 0, 0, 0, 0,
+    0.5f, -0.5f, -0.5f, 0, 1, 0, 1, 0,
+    0.5f, -0.5f, 0.5f, 0, 0, 1, 1, 1,
+    -0.5f, -0.5f, 0.5f, 1, 1, 0, 0, 1};
 
-GLuint indices[] =
-    {
-        // Front
-        0, 1, 2,
-        2, 3, 0,
+GLuint indices[] = {
+    // Front
+    0, 1, 2, 2, 3, 0,
 
-        // Back
-        4, 5, 6,
-        6, 7, 4,
+    // Back
+    4, 5, 6, 6, 7, 4,
 
-        // Right
-        8, 9, 10,
-        10, 11, 8,
+    // Right
+    8, 9, 10, 10, 11, 8,
 
-        // Left
-        12, 13, 14,
-        14, 15, 12,
+    // Left
+    12, 13, 14, 14, 15, 12,
 
-        // Top
-        16, 17, 18,
-        18, 19, 16,
+    // Top
+    16, 17, 18, 18, 19, 16,
 
-        // Bottom
-        20, 21, 22,
-        22, 23, 20};
+    // Bottom
+    20, 21, 22, 22, 23, 20};
 
 int main()
 {
@@ -95,19 +87,19 @@ int main()
     glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
 
     GLFWwindow *window = glfwCreateWindow(width, height, "Minecraft", NULL, NULL);
+
     if (window == NULL)
     {
         std::cout << "Failed to create GLFW window" << std::endl;
         glfwTerminate();
         return -1;
     }
-    glfwMakeContextCurrent(window);
 
+    glfwMakeContextCurrent(window);
     glfwSetWindowSizeLimits(window, width, height, width, height);
     glfwSetWindowSize(window, width, height);
 
-    int actualWindowWidth;
-    int actualWindowHeight;
+    int actualWindowWidth, actualWindowHeight;
     glfwGetWindowSize(window, &actualWindowWidth, &actualWindowHeight);
     std::cout << "Window client size: " << actualWindowWidth << "x" << actualWindowHeight << std::endl;
 
@@ -117,9 +109,7 @@ int main()
     glfwGetFramebufferSize(window, &framebufferWidth, &framebufferHeight);
     glViewport(0, 0, framebufferWidth, framebufferHeight);
 
-    Shader shaderProgram(
-        "src/shaders/default.vert",
-        "src/shaders/default.frag");
+    Shader shaderProgram("src/shaders/default.vert", "src/shaders/default.frag");
 
     GLuint modelLoc = glGetUniformLocation(shaderProgram.ID, "model");
     GLuint tintLoc = glGetUniformLocation(shaderProgram.ID, "tint");
@@ -140,15 +130,21 @@ int main()
 
     Texture GrassBlockTexture_top("assets/textures/block/grass_block_top.png", GL_TEXTURE_2D, GL_TEXTURE0, GL_RGBA, GL_UNSIGNED_BYTE);
     GrassBlockTexture_top.texUnit(shaderProgram, "tex0", 0);
+
     Texture GrassBlockTexture_side("assets/textures/block/grass_block_side.png", GL_TEXTURE_2D, GL_TEXTURE0, GL_RGBA, GL_UNSIGNED_BYTE);
     GrassBlockTexture_side.texUnit(shaderProgram, "tex0", 0);
+
     Texture GrassBlockTexture_bottom("assets/textures/block/dirt.png", GL_TEXTURE_2D, GL_TEXTURE0, GL_RGBA, GL_UNSIGNED_BYTE);
     GrassBlockTexture_bottom.texUnit(shaderProgram, "tex0", 0);
 
     Texture DirtBlockTexture_side("assets/textures/block/dirt.png", GL_TEXTURE_2D, GL_TEXTURE0, GL_RGBA, GL_UNSIGNED_BYTE);
     DirtBlockTexture_side.texUnit(shaderProgram, "tex0", 0);
+
     Texture StoneBlockTexture_side("assets/textures/block/stone.png", GL_TEXTURE_2D, GL_TEXTURE0, GL_RGBA, GL_UNSIGNED_BYTE);
     StoneBlockTexture_side.texUnit(shaderProgram, "tex0", 0);
+
+    Texture BedrockBlockTexture_side("assets/textures/block/bedrock.png", GL_TEXTURE_2D, GL_TEXTURE0, GL_RGBA, GL_UNSIGNED_BYTE);
+    BedrockBlockTexture_side.texUnit(shaderProgram, "tex0", 0);
 
     BlockTexture GrassBlock_Textures = {
         &GrassBlockTexture_top,
@@ -163,27 +159,36 @@ int main()
         &DirtBlockTexture_side,
         &DirtBlockTexture_side,
         &DirtBlockTexture_side,
-        &DirtBlockTexture_side,
-    };
+        &DirtBlockTexture_side};
     BlockTexture StoneBlock_Textures = {
         &StoneBlockTexture_side,
         &StoneBlockTexture_side,
         &StoneBlockTexture_side,
         &StoneBlockTexture_side,
         &StoneBlockTexture_side,
-        &StoneBlockTexture_side,
+        &StoneBlockTexture_side};
+    BlockTexture BedrockBlock_Textures = {
+        &BedrockBlockTexture_side,
+        &BedrockBlockTexture_side,
+        &BedrockBlockTexture_side,
+        &BedrockBlockTexture_side,
+        &BedrockBlockTexture_side,
+        &BedrockBlockTexture_side,
     };
 
     Block GrassBlock = {BlockType::Grass, GrassBlock_Textures};
     Block DirtBlock = {BlockType::Dirt, DirtBlock_Textures};
     Block StoneBlock = {BlockType::Stone, StoneBlock_Textures};
+    Block BedrockBlock = {BlockType::Bedrock, BedrockBlock_Textures};
 
     glEnable(GL_DEPTH_TEST);
+
     glEnable(GL_CULL_FACE);
     glCullFace(GL_BACK);
     glFrontFace(GL_CCW);
 
     Camera camera(framebufferWidth, framebufferHeight, glm::vec3(0.0f, 0.0f, 3.0f));
+
     bool wireframe = false;
     bool wireframeTogglePressed = false;
 
@@ -193,15 +198,18 @@ int main()
     while (!glfwWindowShouldClose(window))
     {
         const bool togglePressed = glfwGetKey(window, GLFW_KEY_V) == GLFW_PRESS;
+
         if (togglePressed && !wireframeTogglePressed)
         {
             wireframe = !wireframe;
             glPolygonMode(GL_FRONT_AND_BACK, wireframe ? GL_LINE : GL_FILL);
         }
+
         wireframeTogglePressed = togglePressed;
 
         glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
         shaderProgram.Activate();
 
         camera.Inputs(window);
@@ -218,98 +226,51 @@ int main()
                     Block *currentBlock;
 
                     if (y == 59)
-                    {
                         currentBlock = &GrassBlock;
-                    }
                     else if (y >= 30)
-                    {
                         currentBlock = &DirtBlock;
-                    }
-                    else
-                    {
+                    else if (y >= 3)
                         currentBlock = &StoneBlock;
-                    }
+                    else
+                        currentBlock = &BedrockBlock;
 
                     glm::mat4 model = glm::mat4(1.0f);
+                    model = glm::translate(model, glm::vec3((float)x, (float)y, (float)z));
 
-                    model = glm::translate(
-                        model,
-                        glm::vec3(
-                            (float)x,
-                            (float)y,
-                            (float)z));
-
-                    glUniformMatrix4fv(
-                        modelLoc,
-                        1,
-                        GL_FALSE,
-                        glm::value_ptr(model));
+                    glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
 
                     // Front
                     currentBlock->textures.front->Bind();
-
-                    glDrawElements(
-                        GL_TRIANGLES,
-                        6,
-                        GL_UNSIGNED_INT,
-                        (void *)0);
+                    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, (void *)0);
 
                     // Back
                     currentBlock->textures.back->Bind();
-
-                    glDrawElements(
-                        GL_TRIANGLES,
-                        6,
-                        GL_UNSIGNED_INT,
-                        (void *)(6 * sizeof(GLuint)));
+                    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, (void *)(6 * sizeof(GLuint)));
 
                     // Right
                     currentBlock->textures.right->Bind();
-
-                    glDrawElements(
-                        GL_TRIANGLES,
-                        6,
-                        GL_UNSIGNED_INT,
-                        (void *)(12 * sizeof(GLuint)));
+                    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, (void *)(12 * sizeof(GLuint)));
 
                     // Left
                     currentBlock->textures.left->Bind();
-
-                    glDrawElements(
-                        GL_TRIANGLES,
-                        6,
-                        GL_UNSIGNED_INT,
-                        (void *)(18 * sizeof(GLuint)));
+                    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, (void *)(18 * sizeof(GLuint)));
 
                     // Top
                     currentBlock->textures.top->Bind();
 
                     if (currentBlock->type == BlockType::Grass)
-                    {
                         glUniform3f(tintLoc, 0.7f, 1.0f, 0.6f);
-                    }
                     else
-                    {
                         glUniform3f(tintLoc, 1.0f, 1.0f, 1.0f);
-                    }
 
-                    glDrawElements(
-                        GL_TRIANGLES,
-                        6,
-                        GL_UNSIGNED_INT,
-                        (void *)(24 * sizeof(GLuint)));
+                    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, (void *)(24 * sizeof(GLuint)));
 
-                    // IMPORTANT: reset after drawing the top
+                    // Reset tint
                     glUniform3f(tintLoc, 1.0f, 1.0f, 1.0f);
 
                     // Bottom
                     currentBlock->textures.bottom->Bind();
-
-                    glDrawElements(
-                        GL_TRIANGLES,
-                        6,
-                        GL_UNSIGNED_INT,
-                        (void *)(30 * sizeof(GLuint)));
+                    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, (void *)(30 * sizeof(GLuint)));
                 }
             }
         }
@@ -324,5 +285,6 @@ int main()
     shaderProgram.Delete();
     glfwDestroyWindow(window);
     glfwTerminate();
+
     return 0;
 }
